d225af88e44dfc9aba5ccdf34e87a7fb12a58fe8
Mohammad Nadiatul Islam Sakib <websakib07@gmail.com>
2025-08-19 16:18:51 +0600

    data types and simple problem solves

--- data-types.js
+++ data-types.js
@@ -1,4 +1,9 @@
-let c = undefined;
-console.log(typeof c);
+// 7 Primitive data types in JS
+
 let a = null;
-console.log(typeof null);
+let b = 123;
+let c = true;
+let d = BigInt(123);
+let e = Symbol("This is a symbol");
+let f = "Sakib";
+let g = undefined;
---
+++ practice.js
@@ -0,0 +1,35 @@
+// p1
+
+let a = "Sakib";
+let b = 8;
+console.log(a + b);
+
+// p2
+
+console.log(typeof (a + b));
+
+// p3
+
+const employee = {
+  name: "Sakib",
+  employeeID: 11113,
+  designation: "Operations Manager",
+};
+
+// p4
+
+employee["team"] = "Shopify Shinobi";
+employee["employeeID"] = "11111";
+
+console.log(employee);
+
+// p5
+
+const dictionary = {
+  rice: "Vat",
+  water: "pani",
+  ice: "borof",
+};
+
+console.log(dictionary.ice);
+console.log(dictionary["ice"]);
