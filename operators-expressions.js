3b9d55650cdb0f90c9d3dc11599d30c7775b9235
nisakib07 <websakib07@gmail.com>
2025-08-20 08:51:41 +0600

    Learning operators

---
+++ operators-expressions.js
@@ -0,0 +1,28 @@
+let a = 7;
+let b = 5;
+
+// Arithmetic Operators
+console.log("a+b = ", a + b);
+console.log("a-b = ", a - b);
+console.log("a*b = ", a * b);
+console.log("a/b = ", a / b);
+console.log("a**b = ", a ** b);
+console.log("a%b = ", a % b);
+
+// Incremental Operator
+console.log("a++", a++);
+// Will come 7
+console.log("a", a);
+// Here comes 8
+
+console.log("++a", ++a);
+// Will become 9 immediately
+
+// Decremental Operator
+console.log("a--", a--);
+//will show 9 as it is
+console.log("a", a);
+// Here shows 8
+
+console.log("--a", --a);
+// Shows 7 immediately
